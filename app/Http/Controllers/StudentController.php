12d3a162e6bc1b2dafac56d5963784cb0226fe12
tapan288 <tapansharma69@gmail.com>
2023-05-01 12:50:37 +0545

    setup CRUD for Student Resource

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::with(['class', 'section'])->get();
+
+        return view('students.index', compact('students'));
+    }
+
+    public function create()
+    {
+        return view('students.create');
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'address',
+        'class_id',
+        'section_id',
+    ];
+
+    protected $with = [
+        'class',
+        'section',
+    ];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
